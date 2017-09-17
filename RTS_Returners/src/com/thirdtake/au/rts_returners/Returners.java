92aaf6552e490b0fb2c4960343b0a2723efa87fd
Oliver Clarke <oliverclarke50@gmail.com>
2017-09-17 14:32:55 +1000

    Added a version ID to the main class.

--- RTS_Returners/src/com/thirdtake/au/rts_returners/Returners.java
+++ RTS_Returners/src/com/thirdtake/au/rts_returners/Returners.java
@@ -1,7 +1,14 @@
 package com.thirdtake.au.rts_returners;
 
+/*
+ * @Author: Oliver Clarke
+ * @Date: 17/09/2017
+ * @Discription:  This is the main method for the Returners RTS game.  It handles the initialization of the applications.
+ */
 public class Returners {
-
+	
+	public static final String BUILD_ID = "v0.1";
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("RTS_Returners starting...");
