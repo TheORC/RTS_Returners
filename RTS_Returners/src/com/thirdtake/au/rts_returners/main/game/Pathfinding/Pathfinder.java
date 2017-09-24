81084c603e2343ace758e36de26d746d534e2c42
Oliver Clarke <oliverclarke50@gmail.com>
2017-09-25 08:52:06 +1000

    Created an AI branch and added a path-finding class.

---
+++ RTS_Returners/src/com/thirdtake/au/rts_returners/main/game/Pathfinding/Pathfinder.java
@@ -0,0 +1,9 @@
+package com.thirdtake.au.rts_returners.main.game.Pathfinding;
+
+/**
+ * @author Oliver Clarke
+ * @ This class uses an A* path-finding system to calculate paths for each unit.
+ */
+public class Pathfinder {
+
+}
