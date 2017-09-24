f169355d3a2fd645d70a1d5e372b2713d8849ff7
Oliver Clarke <oliverclarke50@gmail.com>
2017-09-24 14:53:26 +1000

    Updated Image utils

--- RTS_Returners/src/com/thirdtake/au/rts_returners/main/Utils/Images.java
+++ RTS_Returners/src/com/thirdtake/au/rts_returners/main/Utils/Images.java
@@ -11,18 +11,32 @@ import javax.imageio.ImageIO;
  *
  */
 public class Images {
-	
-	//Declare Images Here
+
+	// Declare Images Here
 	public static BufferedImage baseSprite;
-	
-	public static void load(){
+
+	public static void load() {
 		try {
-			
-			//Initialise Images Here
+
+			// Initialise Images Here
 			baseSprite = ImageIO.read(new File("./resources/buildings/baseSprite.png"));
-	
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	/**
+	 * @param filePath - Path to the file being loaded
+	 * @return The loaded image or null.
+	 */
+	public static BufferedImage LoadImage(String filePath) {
+		BufferedImage image = null;
+		try {
+			image = ImageIO.read(new File(filePath));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return image;
 	}
 }
