9c0593362b2fa45f360447e6b4b7b5419acd198d
Oliver Clarke <oliverclarke50@gmail.com>
2017-09-25 09:07:40 +1000

    Added a reference to the Pathfinding Author.

--- RTS_Returners/src/com/thirdtake/au/rts_returners/main/game/Pathfinding/Pathfinder.java
+++ RTS_Returners/src/com/thirdtake/au/rts_returners/main/game/Pathfinding/Pathfinder.java
@@ -1,8 +1,12 @@
 package com.thirdtake.au.rts_returners.main.game.Pathfinding;
 
 /**
- * @author Oliver Clarke
+ * @author Justin Wetherell <phishman3579@gmail.com>
+ * 
+ * @ Adaptation by Oliver Clarke
  * @ This class uses an A* path-finding system to calculate paths for each unit.
+ *   The original code can be found at: https://github.com/phishman3579/java-algorithms-implementation/blob/master/src/com/jwetherell/algorithms/graph/AStar.java
+ *   This is not the same code, rather an adapted version of it.
  */
 public class Pathfinder {
 
